ebc04e5511d843d57faca3e7d55fc6389742783a
Barthelemy Ducrot <barthelemy.ducrot@hei.fr>
2017-05-25 15:42:06 +0100

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Drop th DB"
+Dose.destroy_all
+Cocktail.destroy_all
+Ingredient.destroy_all
+puts "DB cleared"
+puts "lets create some ingredients"
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+puts "#{Ingredient.all.length} ingredients has been created"
+
+puts "Lets create some cocktails"
+(0..25).to_a.sample.times do
+  cocktail = Cocktail.new
+  cocktail.name = "cocktail #{('a'..'z').to_a.sample.capitalize}"
+  if cocktail.save
+    (0..10).to_a.sample.times do
+      dose = Dose.new
+      dose.ingredient = Ingredient.all.sample
+      dose.cocktail = cocktail
+      dose.description = "take some #{dose.ingredient.name} and put it in the glass"
+      dose.save
+    end
+    puts "#{cocktail.name} recipe has been created"
+  end
+end
+puts "#{Cocktail.all.length} cocktails has been created"
+
+
